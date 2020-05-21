b013c53d3cf98b55b47ed372cdda3883a0793c42
neerajsharma <neerajs@jivox.com>
2020-05-21 15:34:38 +0530

    String expression validation in java

---
+++ DSJAVA/src/com/ds/java/Expression.java
@@ -0,0 +1,36 @@
+package com.ds.java;
+
+import java.util.Stack;
+
+public class Expression {
+
+	public static void main(String[] args) {
+		 
+         String str = "({[]})";
+ 
+         //Declare a stack
+         Stack<Character> st = new Stack<Character>();
+ 
+         //Traverse a string
+         for(int i = 0; i < str.length(); i++) {
+ 
+            if(str.charAt(i) == '{' || str.charAt(i) == '[' || str.charAt(i) == '(') {
+                   st.push(str.charAt(i));
+ 
+            } else if ( !st.empty() && ((str.charAt(i) == ']' && st.peek() == '[') || (str.charAt(i) == '}' && st.peek() == '{') ||(str.charAt(i) == ')' && st.peek() == '('))) {
+ 
+                   st.pop();
+ 
+             } else {
+                  st.push(str.charAt(i));
+             }
+         }
+ 
+         if(st.empty()) {
+              System.out.println("Balanced");
+         } else {
+              System.out.println("Not balanced");
+         }
+   }
+
+}
