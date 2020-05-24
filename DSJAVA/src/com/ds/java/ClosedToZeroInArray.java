154df2f4f893270faa120d7e1923810ce22da93e
neerajsharma <neerajs@jivox.com>
2020-05-24 09:43:47 +0530

    fine Close to zero num in Array

--- DSJAVA/src/com/ds/java/ClosedToZeroInArray.java
+++ DSJAVA/src/com/ds/java/ClosedToZeroInArray.java
@@ -5,19 +5,34 @@ public class ClosedToZeroInArray {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int arr[]={1,3,-5,7,8,20,-40,6};
-		int pairIndex1=0;
-		int pairIndex2=1;
+		int index1=0;
+		int index2=0;
 		int minSum=arr[0]+arr[1];
 		for(int i=0;i<arr.length;i++) {
 			for(int j=i+1;j<arr.length;j++) {
 				int tempSum=arr[i]+arr[j];
 				if(Math.abs(tempSum)<Math.abs(minSum)) {
-					pairIndex1=i;
-					pairIndex2=j;
+					index1=i;
+					index2=j;
 					minSum=tempSum;
+					
 				}
 			}
 		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
