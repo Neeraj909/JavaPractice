659852b9d3ce07056776c70f78cbe7a0e335494b
neerajsharma <neerajs@jivox.com>
2020-09-30 14:26:20 +0530

    Binnary to Decimal

---
+++ DSJAVA/src/com/ds/java/BinnaryToDecimal.java
@@ -0,0 +1,5 @@
+package com.ds.java;
+
+public class BinnaryToDecimal {
+
+}
--- DSJAVA/src/com/ds/java/StringAlternatCharcter.java
+++ DSJAVA/src/com/ds/java/StringAlternatCharcter.java
@@ -1,5 +1,26 @@
 package com.ds.java;
 
-public class StringAlternatCharcter {
+import java.util.Scanner;
 
+public class StringAlternatCharcter {
+	 public static void main(String args[] ) throws Exception {
+
+	Scanner scn=new Scanner(System.in);
+    String str=scn.nextLine();
+    int[] occ = new int[122]; 
+
+  String s = str.toLowerCase();
+
+  for (int i = 0; i < str.length(); i++)  
+  { 
+        
+      char temp = s.charAt(i); 
+      occ[temp]++; 
+
+      if (occ[temp]%2 != 0) 
+          System.out.print(str.charAt(i)); 
+  } 
+  System.out.println();
+  
+}
 }
