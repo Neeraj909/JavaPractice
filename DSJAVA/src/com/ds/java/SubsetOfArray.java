f0f449cac6458245cdbce3893b445d9d5dab8124
neerajsharma <neerajs@jivox.com>
2020-09-30 14:18:31 +0530

    StringAlterNateCharcterSTring

--- DSJAVA/src/com/ds/java/SubsetOfArray.java
+++ DSJAVA/src/com/ds/java/SubsetOfArray.java
@@ -26,23 +26,15 @@ public class SubsetOfArray {
 		combinationUtil(arr, n, r, index, data, i + 1);
 	}
 
-// The main function that prints all combinations 
-// of size r in arr[] of size n. This function  
-// mainly uses combinationUtil() 
 	static void printCombination(int arr[], int n, int r) {
-// A temporary array to store all combination 
-// one by one 
 		int data[] = new int[r];
-
-// Print all combination using temprary 
-// array 'data[]' 
-		combinationUtil(arr, n, r, 0, data, 0);
+		combinationUtil(arr, n, r, 0, data, 0);	
 	}
 
 	/* Driver function to check for above function */
 	public static void main(String[] args) {
 		int arr[] = { 10, 20, 30, 40, 50 };
-		int r = 3;
+		int r = 2;
 		int n = arr.length;
 		printCombination(arr, n, r);
 	}
