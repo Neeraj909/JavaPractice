14c6b96ba18b5447dfc16819c3d3b1a3bbe07e1b
neerajsharma <neerajs@jivox.com>
2020-10-07 18:29:27 +0530

    printSubString

--- DSJAVA/src/com/ds/java/BinnaryToDecimal.java
+++ DSJAVA/src/com/ds/java/BinnaryToDecimal.java
@@ -1,5 +1,28 @@
 package com.ds.java;
 
+import java.util.Scanner;
+
 public class BinnaryToDecimal {
+	public static void main(String args[] ) throws Exception {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+         Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        System.out.println(binaryToDecimal(n));
+    }
+    static int binaryToDecimal(int n) 
+    { 
+        int num = n; 
+        int dec_value = 0; 
+        int base = 1; 
+        int temp = num; 
+        while (temp > 0) { 
+            int last_digit = temp % 10; 
+            temp = temp / 10; 
+            dec_value += last_digit * base; 
+            base = base * 2; 
+        } 
+  
+        return dec_value; 
+    } 
 
 }
---
+++ DSJAVA/src/com/ds/java/PrintsubString.java
@@ -0,0 +1,28 @@
+package com.ds.java;
+
+public class PrintsubString {
+	static void subString(char str[], int n) { 
+        // Pick starting point 
+        for (int len = 1; len <= n; len++) { 
+            // Pick ending point 
+            for (int i = 0; i <= n - len; i++) { 
+                //  Print characters from current 
+                // starting point to current ending 
+                // point.   
+                int j = i + len - 1; 
+                for (int k = i; k <= j; k++) { 
+                    System.out.print(str[k]); 
+                } 
+  
+                System.out.println(); 
+            } 
+        } 
+    } 
+  
+// Driver program to test above function 
+    public static void main(String[] args) { 
+        char str[] = {'a', 'b', 'c'}; 
+        subString(str, str.length); 
+  
+    } 
+}
