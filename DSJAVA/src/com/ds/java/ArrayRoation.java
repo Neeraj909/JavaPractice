73d9e2a1cc87407458347dd980f6fcaecf38e5ed
neerajsharma <neerajs@jivox.com>
2020-11-12 13:39:46 +0530

    Array shifting element

--- DSJAVA/src/com/ds/java/ArrayRoation.java
+++ DSJAVA/src/com/ds/java/ArrayRoation.java
@@ -26,6 +26,20 @@ public class ArrayRoation {
 	        
 
 	    }
+	    int arr[]={1,2,0,0,1,2,3};
+     int count=0;
+              for(int i=0;i<arr.length;i++){
+                  if(arr[i]==0){
+                      for(int j=i;j>0;j--){
+                          int num=arr[j];
+                          arr[j]=arr[j-1];
+                          arr[j-1]=num;
+                      }
+                  }
+              }
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
 	    
 	
 }
